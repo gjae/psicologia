967c10b6da1616928c47c9aebe3c7fbbe05ec43d
pablomoyaseo <ing.pmoya@gmail.com>
2023-04-26 16:21:16 -0400

    pagina 403 y pagina 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,24 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Error 403</title>
+    <title>Error 404</title>
     <link rel="shortcut icon" href="../public/images/logo.png" type="image/png">
     <link rel="stylesheet" href="{{ asset('vendor/pablo/style.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+    
 </head>
 <body>
     <div class="container-fluid">
         <div class="row justify-content-around align-items-center order-reverse">
             <div class="col-12 col-lg-5">
                 <img class="img-responsive d-block mx-auto py-5 py-lg-0" src="../public/images/psicologo-monterrico.png" alt="">
-                <h1 class="h2 text-center py-3">Error 403</h1>
-                <h2 class="h4 text-center">Lo sentimos, usted no tiene permiso para ver esta ruta.</h2>
-                <h2 class="h4 text-center text-primary">Por favor, Dirigase a nuestra página de Inicio para continuar navegando en nuestro sitio web.</h2>
+                <h1 class="h2 text-center py-3">Pagina 404 No encontrada</h1>
+                <h2 class="h4 text-center">Lo sentimos, pero no hemos encontrado su página, posiblemente hay un error en la dirección de su ruta. </h2>
+                <h2 class="h4 text-center text-primary">Por favor, Dirigase a nuestra página de Inicio.</h2>
 
                 <a class="btn btn-primary text-uppercase d-block mx-auto py-3 mt-3 mb-5" href="http://registro.psicologomonterrico.pe/">Página de Inicio</a>
 
             </div>
-            <div class="col-12 col-lg-5 error-imagen-403">
+            <div class="col-12 col-lg-5 error-imagen-404">
 
             </div>
         </div>
